4c2d4268c782bddfde4e23b7018a42d94ec51bfc
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2024-05-15 16:25:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,8 +38,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/vip_82bets")
-SUPPORT_CHAT = getenv("💰 Lottery 💰", "https://82bet.com/#/register?invitationCode=117271407848")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/learningbots79")
+SUPPORT_CHAT = getenv("SUPPORT_GROUP", "https://t.me/learning_bots")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))

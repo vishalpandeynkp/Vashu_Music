ee9fefdb169ba74d3d3b1bacf1b612c313b16d2a
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2024-05-16 08:58:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://t.me/FRIENDSHIP_CHAT_GROUP0",
+    "UPSTREAM_REPO", "https://github.com/Learningbots79/LB_Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
